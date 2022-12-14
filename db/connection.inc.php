a79bc8bd725f709aa9b6bd072586ddefb3b9e5c2
elrick046 <87386997+elrick046@users.noreply.github.com>
2022-12-14 15:12:58 -0300

    testes

--- db/connection.inc.php
+++ db/connection.inc.php
@@ -1,8 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "0406";
+$password = "260405";
 $dbname = "db_to_do";
 
 // Connect to the MySQL database using the PDO object.
 $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+//erick
